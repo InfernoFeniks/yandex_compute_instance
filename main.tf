968ff729a7698802b0564b01d789ef6bcfb7a9c3
InfernoFeniks <70452034+InfernoFeniks@users.noreply.github.com>
2023-07-25 21:57:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
-locals {
-  labels = length(keys(var.labels)) >0 ? var.labels: {
-    "env"=var.env_name
-    "project"="undefined"
-  }
-}
-
-
-
-
 data "yandex_compute_image" "my_image" {
   family = var.image_family
 }
