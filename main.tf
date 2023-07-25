74c55bc7b24ac3cadfac4e6669b385179c84a699
InfernoFeniks <70452034+InfernoFeniks@users.noreply.github.com>
2023-07-25 21:59:29 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+locals {
+  labels = length(keys(var.labels)) >0 ? var.labels: {
+    "env"=var.env_name
+    "project"="undefined"
+  }
+}
+
 data "yandex_compute_image" "my_image" {
   family = var.image_family
 }
