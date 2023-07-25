4d671c3f6cea9bb1db34e7c0dda56b47f8781aad
InfernoFeniks <70452034+InfernoFeniks@users.noreply.github.com>
2023-07-25 21:59:15 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
-provider "template" {
-  version = "2.2.0"
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+    provider "template" {
+      source = "hashicorp/template
+      version = ">= 2.0.0"
+    }
+  }
+  required_version = ">= 0.13"
 }
